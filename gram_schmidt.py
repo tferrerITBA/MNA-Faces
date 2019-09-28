d52df389419d1c9fab3fd64c4291c3ab4c75a98b
tferrerITBA <tferrer@itba.edu.ar>
2019-09-28 16:08:59 -0300

    Fix Gram-Schmidt

--- gram_schmidt.py
+++ gram_schmidt.py
@@ -6,20 +6,17 @@ def gram_schmidt(A):
     Q = np.zeros(shape=(n, n))
 
     for k in range(0, n):
-        print("m: {}; k: {}; A[0:m,k] {}\n\n".format(m, k, A[0:m,k]))
         R[k,k] = np.linalg.norm(A[0:m,k])
         Q[0:m,k] = A[0:m,k] / R[k,k]
 
         for j in range(k+1, n):
-            print("Q[0:m,k]: {}; A[0:m,j]: {}\n".format(Q[0:m,k], A[0:m,j]))
-            print("Result: {}\n".format(np.dot(np.transpose(Q[0:m,k]), A[0:m,j])))
             R[k,j] = np.dot(np.transpose(Q[0:m,k]), A[0:m,j])
-            A[0:m,j] = A[0:m,j] - Q[0:m,k] * R[k,j]
+            A[0:m,j] = A[0:m,j] - np.dot(Q[0:m,k], R[k,j])
 
     return (Q,R)
 
-    print("{}".format(R))
 
-
-A = np.ones(shape=(4,4))
-gram_schmidt(A)
+A = np.array([[1.,2.], [3.,4.]])
+Q, R = gram_schmidt(A)
+print("Q {}".format(Q))
+print("R {}".format(R))
